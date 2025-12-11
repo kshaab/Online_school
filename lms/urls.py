7d5e1007d57118f1bdbeeda4cc0b737ce1a48bb3
Ксения <kssshaab@gmail.com>
2025-12-11 15:19:25 +0300

    add views

---
+++ lms/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+from lms.apps import LmsConfig
+from lms.views import CourseViewSet,LessonCreateApiView, LessonUpdateApiView, LessonListApiView, LessonRetrieveApiView, LessonDestroyApiView
+
+app_name = LmsConfig
+
+router = SimpleRouter()
+router.register("", CourseViewSet)
+
+urlpatterns = [
+    path("lesson/create/", LessonCreateApiView.as_view(), name="lesson_create"),
+    path("lesson/list/", LessonListApiView.as_view(), name="lesson_list"),
+    path("lesson/<int:pk>/", LessonRetrieveApiView.as_view(), name="lesson_retrieve"),
+    path("lesson/<int:pk>/update", LessonUpdateApiView.as_view(), name="lesson_update"),
+    path("lesson/<int:pk>/delete", LessonDestroyApiView.as_view(), name="lesson_delete"),
+]
+
+urlpatterns += router.urls
--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from lms.models import Course, Lesson
+from lms.serializers import CourseSerializer, LessonSerializer
+from rest_framework import generics
 
-# Create your views here.
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class LessonCreateApiView(generics.CreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+class LessonListApiView(generics.ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+class LessonRetrieveApiView(generics.RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+class LessonUpdateApiView(generics.UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+class LessonDestroyApiView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
